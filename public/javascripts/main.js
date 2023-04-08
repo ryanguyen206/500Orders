76fc0aa9a3563166c127c2234b0e4705c52ac3dd
ryanguyen206 <ryanninja595@gmail.com>
2023-04-07 18:23:57 -0700

    currently can generate 500 orders

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,62 +1,81 @@
 
-let movieArray = [];
+let orderArray = [];
 
+let storeIDArray = [98053, 98007, 98077, 98055, 98011, 98046]
+
+let cdIDArray = [123456, 123654, 321456, 321654, 654123, 654321, 543216, 354126, 621453, 623541]
+
+let pricePaidArray = [1,2,3,4,5,6,7,8,9]
+
+let salesPersonOne = [1,2,3,4]
+let salesPersonTwo = [5,6,7,8]
+let salesPersonThree = [9,10,11,12]
+let salesPersonFour = [13,14,15,16]
+let salesPersonFive = [17,18,19,20]
+let salesPersonSix = [21,22,23,24]
 // define a constructor to create movie objects
-let MovieObject = function (pTitle, pYear, pGenre, pMan, pWoman, pURL) {
-    this.ID = Math.random().toString(16).slice(5)  // tiny chance could get duplicates!
-    this.Title = pTitle;
-    this.Year = pYear;
-    this.Genre = pGenre;  // action  comedy  drama  horrow scifi  musical  western
+let OrderObject = function (pStoreID, pSalesPersonID, pCdID, pPricePaid, pDate) {
+  
+    this.storeID = pStoreID;
+    this.salesPersonID = pSalesPersonID;
+    this.cdID = pCdID;  
+    this.pricePaid = pPricePaid; 
+    this.date = pDate; 
 }
 
 let selectedGenre = "not selected";
 
 document.addEventListener("DOMContentLoaded", function () {
 
-    createList();
+ 
 
 // add button events ************************************************************************
-    
+    document.getElementById("dummyButtonAdd").addEventListener("click", function() {
+        let storeID = storeIDArray[Math.floor(Math.random() * storeIDArray.length)];
+        let salesID = determineSalesID(storeID);
+        let cdID = cdIDArray[Math.floor(Math.random() * cdIDArray.length)];
+        let pricePaid = pricePaidArray[Math.floor(Math.random() * pricePaidArray.length)];
+        
+        var t = new Date();
+        t.setSeconds(t.getSeconds() + Math.floor(Math.random() * 30));
+
+
+        console.log(storeID, salesID, cdID, pricePaid, t)
+        
+        
+    })
+//button one - 
     document.getElementById("buttonAdd").addEventListener("click", function () {
-        let newMovie = new MovieObject(document.getElementById("title").value, 
-        document.getElementById("year").value, selectedGenre);
+        
+        orderArray = [];
 
-        fetch('/AddMovie', {
-            method: "POST",
-            body: JSON.stringify(newMovie),
-            headers: {"Content-type": "application/json; charset=UTF-8"}
-            })
-            .then(response => response.json()) 
-            .then(json => console.log(json),
-            createList()
-            )
-            .catch(err => console.log(err));
+        for (let i=0; i<500; i++) {
+           let storeID = storeIDArray[Math.floor(Math.random() * storeIDArray.length)];
+        let salesID = determineSalesID(storeID);
+        let cdID = cdIDArray[Math.floor(Math.random() * cdIDArray.length)];
+        let pricePaid = pricePaidArray[Math.floor(Math.random() * pricePaidArray.length)];
+        
+        var t = new Date();
+        t.setSeconds(t.getSeconds() + Math.floor(Math.random() * 30));
+            
+            let newOrder = new OrderObject(storeID, salesID, cdID, pricePaid, t)
     
-        // $.ajax({
-        //     url : "/AddMovie",
-        //     type: "POST",
-        //     data: JSON.stringify(newMovie),
-        //     contentType: "application/json; charset=utf-8",
-        //      success: function (result) {
-        //         console.log(result);
-        //         createList();
-        //     }
-        // });
-       
+            fetch('/AddOrder', {
+                method: "POST",
+                body: JSON.stringify(newOrder),
+                headers: {"Content-type": "application/json; charset=UTF-8"}
+                })
+                .then(response => response.json()) 
+                .then(json => console.log(json),
+                )
+                .catch(err => console.log(err));
+        }
+       createList();
     });
 
-    document.getElementById("buttonGet").addEventListener("click", function () {
-        createList();      
-    });
+  
 
-    document.getElementById("buttonDelete").addEventListener("click", function () {
-        deleteMovie(document.getElementById("deleteID").value);      
-    });
     
-    document.getElementById("buttonClear").addEventListener("click", function () {
-        document.getElementById("title").value = "";
-        document.getElementById("year").value = "";
-    });
 
     $(document).bind("change", "#select-genre", function (event, ui) {
         selectedGenre = $('#select-genre').val();
@@ -67,38 +86,72 @@ document.addEventListener("DOMContentLoaded", function () {
 });  
 // end of wait until document has loaded event  *************************************************************************
 
+function determineSalesID(storeID) {
+    let salesID = 0;
+    let length = salesPersonFive.length;
+    98053, 98007, 98077, 98055, 98011, 98046
+    switch(storeID) {
+        case 98053:
+          salesID = salesPersonOne[Math.floor(Math.random() * length)];
+          break;
+        case 98007:
+            salesID = salesPersonTwo[Math.floor(Math.random() * length)];
+          break;
+        case 98077:
+            salesID = salesPersonThree[Math.floor(Math.random() * length)];
+          break;
+        case 98055:
+            salesID = salesPersonFour[Math.floor(Math.random() * length)];
+            break;
+        case 98011:
+            salesID = salesPersonFive[Math.floor(Math.random() * length)];
+                // code block
+                break;
+        case 98046:
+            salesID = salesPersonSix[Math.floor(Math.random() * length)];
+            // code block
+            break;
+        default:
+          return 0;
+      }
+
+      return salesID;
+}
 
 function createList() {
 // update local array from server
 
-    fetch('/getAllMovies')
+    fetch('/getAllOrders')
     // Handle success
     .then(response => response.json())  // get the data out of the response object
     .then( responseData => fillUL(responseData))    //update our array and li's
     .catch(err => console.log('Request Failed', err)); // Catch errors
 
-    // $.get("/getAllMovies", function(data, status){  // AJAX get
-    //     movieArray = data;  // put the returned server json data into our local array
+    $.get("/getAllOrders", function(data, status){  // AJAX get
+        orderArray = data;  // put the returned server json data into our local array
         
-    //       // clear prior data
-    //     var divMovieList = document.getElementById("divMovieList");
-    //     while (divMovieList.firstChild) {    // remove any old data so don't get duplicates
-    //         divMovieList.removeChild(divMovieList.firstChild);
-    //     };
+          // clear prior data
+        var divMovieList = document.getElementById("divMovieList");
+        while (divMovieList.firstChild) {    // remove any old data so don't get duplicates
+            divMovieList.removeChild(divMovieList.firstChild);
+        };
 
-    //     var ul = document.createElement('ul');
+        var ul = document.createElement('ul');
 
-    //     movieArray.forEach(function (element,) {   // use handy array forEach method
-    //         var li = document.createElement('li');
-    //         li.innerHTML = element.ID + ":  &nbsp &nbsp  &nbsp &nbsp " + 
-    //         element.Title + "  &nbsp &nbsp  &nbsp &nbsp "  
-    //         + element.Year + " &nbsp &nbsp  &nbsp &nbsp  " + element.Genre;
-    //         ul.appendChild(li);
-    //     });
-    //     divMovieList.appendChild(ul)
+        orderArray.forEach(function (element,) {   // use handy array forEach method
+            var li = document.createElement('li');
+            li.innerHTML = element.storeID + ":  &nbsp &nbsp  &nbsp &nbsp " + 
+            element.salesPersonID + "  &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp "  
+            + element.cdID + " &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp  " + element.pricePaid 
+            + " &nbsp &nbsp  &nbsp &nbsp  &nbsp &nbsp " + element.date;
+            ul.appendChild(li);
+        });
+        divMovieList.appendChild(ul)
 
-    // });
+    });
 };
+
+
 
 function fillUL(data) {
         // clear prior data
@@ -108,43 +161,22 @@ function fillUL(data) {
     };
 
     var ul = document.createElement('ul');
-    movieArray = data;
-    movieArray.forEach(function (element,) {   // use handy array forEach method
+    orderArray = data;
+    orderArray.forEach(function (element,) {   // use handy array forEach method
         var li = document.createElement('li');
-        li.innerHTML = element.ID + ":  &nbsp &nbsp  &nbsp &nbsp " + 
-        element.Title + "  &nbsp &nbsp  &nbsp &nbsp "  
-        + element.Year + " &nbsp &nbsp  &nbsp &nbsp  " + element.Genre;
+        li.innerHTML = element.storeID + " &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp " + 
+        element.salesPersonID + "  &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp "  
+        + element.cdID + " &nbsp &nbsp  &nbsp &nbsp  " + element.pricePaid 
+        + " &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp  " + element.date;
         ul.appendChild(li);
     });
     divMovieList.appendChild(ul)
 }
 
-function deleteMovie(ID) {
-
-    fetch('/DeleteMovie/' + ID, {
-        method: "DELETE",
-       // body: JSON.stringify(_data),
-        headers: {"Content-type": "application/json; charset=UTF-8"}
-      })
-      .then(response => response.json()) 
-      .then(json => console.log(json))
-      .catch(err => console.log(err));
 
 
-
-    // $.ajax({
-    //     type: "DELETE",
-    //     url: "/DeleteMovie/" +ID,
-    //     success: function(result){
-    //         alert(result);
-    //         createList();
-    //     },
-    //     error: function (xhr, textStatus, errorThrown) {  
-    //         alert("Server could not delete Movie with ID " + ID)
-    //     }  
-    // });
    
-}
+
 
 
   
--- routes/index.js
+++ routes/index.js
@@ -4,43 +4,34 @@ var router = express.Router();
 var fs = require("fs");
 
 // start by creating data so we don't have to type it in each time
-let ServerMovieArray = [];
+let ServerOrderArray = [];
 
 // define a constructor to create movie objects
-let MovieObject = function (pTitle, pYear, pGenre, pMan, pWoman, pURL) {
-    this.ID = Math.random().toString(16).slice(5)  // tiny chance could get duplicates!
-    this.Title = pTitle;
-    this.Year = pYear;
-    this.Genre = pGenre;  // action  comedy  drama  horrow scifi  musical  western
+let OrderObject = function (pStoreID, pSalesPersonID, pCdID, pPricePaid, pDate) {
+  
+    this.storeID = pStoreID;
+    this.salesPersonID = pSalesPersonID;
+    this.cdID = pCdID;  
+    this.pricePaid = pPricePaid; 
+    this.date = pDate; // action  comedy  drama  horrow scifi  musical  western
 }
 
 // my file management code, embedded in an object
 fileManager  = {
-
   // this will read a file and put the data in our movie array
   // NOTE: both read and write files are synchonous, we really can't do anything
   // useful until they are done.  If they were async, we would have to use call backs.
   // functions really should take in the name of a file to be more generally useful
   read: function() {
-    // has extra code to add 4 movies if and only if the file is empty
-    const stat = fs.statSync('moviesData.json');
-    if (stat.size !== 0) {                           
-    var rawdata = fs.readFileSync('moviesData.json'); // read disk file
-    ServerMovieArray = JSON.parse(rawdata);  // turn the file data into JSON format and overwrite our array
-    }
-    else {
-      // make up 3 for testing
-      ServerMovieArray.push(new MovieObject("Moonstruck", 1981, "Drama"));
-      ServerMovieArray.push(new MovieObject("Wild At Heart", 1982, "Drama"));
-      ServerMovieArray.push(new MovieObject("Raising Arizona", 1983, "Comedy"));
-      ServerMovieArray.push(new MovieObject("USS Indianapolis", 2016, "Drama"));
-      fileManager.write();
-    }
-  },
+    // has extra code to add 4 movies if and only if the file is empty                        
+    var rawdata = fs.readFileSync('ordersData.json'); // read disk file
+    ServerOrderArray = JSON.parse(rawdata);  // turn the file data into JSON format and overwrite our array
+    },
+   
   
   write: function() {
-    let data = JSON.stringify(ServerMovieArray);    // take our object data and make it writeable
-    fs.writeFileSync('moviesData.json', data);  // write it
+    let data = JSON.stringify(ServerOrderArray);    // take our object data and make it writeable
+    fs.writeFileSync('ordersData.json', data);  // write it
   },
 }
 
@@ -51,17 +42,17 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET all Movie data */
-router.get('/getAllMovies', function(req, res) {
+router.get('/getAllOrders', function(req, res) {
   fileManager.read();
-  res.status(200).json(ServerMovieArray);
+  res.status(200).json(ServerOrderArray);
 });
 
 
 /* Add one new Movie */
-router.post('/AddMovie', function(req, res) {
-  const newMovie = req.body;  // get the object from the req object sent from browser
-  console.log(newMovie);
-  ServerMovieArray.push(newMovie);  // add it to our "DB"  (array)
+router.post('/AddOrder', function(req, res) {
+  const newOrder = req.body;  // get the object from the req object sent from browser
+  console.log(newOrder);
+  ServerOrderArray.push(newOrder);  // add it to our "DB"  (array)
   fileManager.write();
   // prepare a reply to the browser
   var response = {
@@ -73,34 +64,7 @@ router.post('/AddMovie', function(req, res) {
 
 // delete movie
 
-router.delete('/DeleteMovie/:ID', (req, res) => {
-  const ID = req.params.ID;
-  let found = false;
-  console.log(ID);    
 
-  for(var i = 0; i < ServerMovieArray.length; i++) // find the match
-  {
-      if(ServerMovieArray[i].ID === ID){
-        ServerMovieArray.splice(i,1);  // remove object from array
-          found = true;
-          fileManager.write();
-          break;
-      }
-  }
-
-  if (!found) {
-    console.log("not found");
-    return res.status(500).json({
-      status: "error"
-    });
-  } else {
-    var response = {
-      status  : 200,
-      success : 'Movie ' + ID + ' deleted!'
-    }
-    res.end(JSON.stringify(response)); // send reply
-  }
-});
 
 
 module.exports = router;
